452616126dc886c1d7e7630745bad8aac0e2d9bf
triasamo1 <triasam10@gmail.com>
2022-06-14 19:06:21 +0300

    Remove junk blocks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,5 @@
-# resource  "aws_vpc" "sandbox-vpc" {
-#     cidr_block = var.vpc_cidr_block
-# }
-
-# module "network" {
-#     source = "./modules/network"
-#     my_ip = var.my_ip
-# }
-
 module "lambda_function"{
     source = "./modules/lambda"
-    # security_group_id = module.network.security_group_id
-    # subnet_id = module.network.subnet_id
 }
 
 module "api_backend"{
@@ -18,7 +7,6 @@ module "api_backend"{
     account_id = var.account_id
     lambda_function_arn = module.lambda_function.lambda_function_arn
     lambda_function_invoke_arn = module.lambda_function.lambda_function_invoke_arn
-    # vpc_endpoint_id = module.network.vpc_endpoint_id
 
     depends_on = [
         module.lambda_function
